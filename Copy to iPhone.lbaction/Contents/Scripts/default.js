a2f99eea97bca02b240a23e56c2fdd5f1cb3985f
Padraic Renaghan <prenagha@renaghan.com>
2014-04-30 13:33:46 -0400

    updated for beta 5

--- Copy to iPhone.lbaction/Contents/Scripts/default.js
+++ Copy to iPhone.lbaction/Contents/Scripts/default.js
@@ -1,30 +1,43 @@
 
-// The 'run' function is called by LaunchBar when the user opens the action.
-function run(arg) {
-    try {
-        // copy from clipboard, unless arg is passed
-        var action = 'copy';
-        var txt = null;        
-        if (arg) {
-          txt = arg;
-          action = 'copyText';
-        }
-        
-        // The Action.preferences object is persistent across runs of the action. 
-        // ~/Library/Application Support/LaunchBar/Action Support/<actionBundleID>/Preferences.plist
-        if (Action.preferences.device == undefined) {
-            // The device wasn't set in the action's preferences, fall back to to iPhone
-            Action.preferences.device = 'iPhone';
-        }
+function runWithString(string) {
+  go(string);
+}
 
-		    var url = 'command-c://x-callback-url/' + action + '?deviceName=' + encodeURIComponent(Action.preferences.device);
-        if (txt && txt.length > 0) {
-		      url = url + '&text=' + encodeURIComponent(txt);
-		    }
-        LaunchBar.debugLog('URL=' + url);
-		    LaunchBar.openURL(url);
+function runWithPaths(paths) {
+  var text = "";
+  for (var i in paths) {
+    try {
+      text = text + File.readText(paths[i]);
     } catch (exception) {
-        LaunchBar.log('Error ' + exception);
-        LaunchBar.alert('Error copying to iPhone', exception);
+      LaunchBar.log('Error reading file ' + paths[i] + ' -- ' + exception);
+      LaunchBar.alert('Error copying to iPhone, cannot read file ' + paths[i], exception);
     }
+  }
+  go(text);
+}
+
+function run() {
+  go(LaunchBar.getClipboardString());
+}
+
+function go(text) {
+  if (!text || text == undefined || text.length == 0) {
+    LaunchBar.alert('No text found to send to iPhone');
+    return;
+  }
+  try {
+    // The Action.preferences object is persistent across runs of the action. 
+    // ~/Library/Application Support/LaunchBar/Action Support/<actionBundleID>/Preferences.plist
+    if (Action.preferences.device == undefined) {
+      // The device wasn't set in the action's preferences, fall back to default
+      Action.preferences.device = 'iPhone';
+    }
+
+    var url = 'command-c://x-callback-url/copyText?deviceName=' + encodeURIComponent(Action.preferences.device) + '&text=' + encodeURIComponent(text);
+    LaunchBar.debugLog('URL=' + url);
+    LaunchBar.openURL(url);
+  } catch (exception) {
+    LaunchBar.log('Error ' + exception);
+    LaunchBar.alert('Error copying to iPhone', exception);
+  }
 }
