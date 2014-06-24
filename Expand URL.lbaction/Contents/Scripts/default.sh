1d7772ae6d1ffba5195b628b146cfad918bef6f5
Padraic Renaghan <prenagha@renaghan.com>
2014-06-24 10:50:28 -0400

    fix json return

--- Expand URL.lbaction/Contents/Scripts/default.sh
+++ Expand URL.lbaction/Contents/Scripts/default.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # original idea from https://gist.github.com/Zettt/88ef3112c04ebecf475b
-EXP=`curl -siL "$1" | grep ^[lL]ocation | tail -n 1 | cut -c 11-`
-# sent bug to LB, returning JSON from script action not working
-#echo "[{'title':'$EXP','url':'$EXP','quickLookURL':'$EXP'}]"
-echo $EXP
+EXP=`curl -siL "$1" | grep ^[lL]ocation | tail -n 1 | cut -c 11- | tr -d '\n' | tr -d '\r'`
+echo "[{\"title\":\"$EXP\",\"subtitle\":\"$1\",\"url\":\"$EXP\",\"quickLookURL\":\"$EXP\"}]"
 exit 0
