67d0c9abd8f76a775fbd2212316a480862f30099
Padraic Renaghan <prenagha@renaghan.com>
2015-11-11 11:05:50 -0600

    jira search

--- JIRA.lbaction/Contents/Scripts/jira.js
+++ JIRA.lbaction/Contents/Scripts/jira.js
@@ -1,7 +1,26 @@
 
+var HOST = "http://cscerdevnor001.fsg.amer.csc.com";
+var JIRA_HOST = HOST + ":8080";
+var CRUCIBLE_HOST = HOST + ":8060";
+
 function run(arg) {
-  var term = encodeURIComponent(arg);
-  var url = "http://cscerdevnor001.fsg.amer.csc.com:8080/issues/?jql=(summary%20~%20%22" + term + "%22%20or%20description%20~%20%22" + term + "%22)%20and%20status%20!%3D%20Closed%20order%20by%20key%20desc";
+  if (!arg || arg === null || typeof(arg) != 'string' || arg.length === 0)
+    return;
+  
+  var url = "";
+  if (arg.match(/^\d+$/)) {
+    url = JIRA_HOST + "/browse/ER-" + arg;
+  } else if (arg.match(/^ER-\d+$/i)) {
+    url = JIRA_HOST + "/browse/" + arg;
+  } else if (arg.match(/^CR-\d+$/i)) {
+    url = CRUCIBLE_HOST + "/cru/" + arg;
+  } else {
+    var term = encodeURIComponent(arg);
+    url = JIRA_HOST + "/issues/?jql=(summary%20~%20%22" 
+      + term + "%22%20or%20description%20~%20%22" 
+      + term + "%22)%20and%20status%20!%3D%20Closed%20order%20by%20key%20desc";
+  }
+      
   LaunchBar.debugLog("URL " + url);
   LaunchBar.openURL(url);
 }
