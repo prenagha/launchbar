e6ba50656ffe14146de9284715b604f28df0e6c0
Padraic Renaghan <prenagha@renaghan.com>
2017-07-28 16:14:09 -0400

    update prefs to be mins

--- Generate Password.lbaction/Contents/Scripts/generate.js
+++ Generate Password.lbaction/Contents/Scripts/generate.js
@@ -4,6 +4,7 @@ var TYPES = "LUNS";
 function setup() {
   // setup default preferences if missing
   // standard characters WITHOUT easy to mistake 0Oo, iI1lL
+  // set a *Min value to -1 to mean NONE of those characters
   if (!Action.preferences.lowercase)
     Action.preferences.lowercase = 'abcdefghjkmnpqrstuvwxyz';
   if (!Action.preferences.uppercase)
@@ -14,10 +15,14 @@ function setup() {
     Action.preferences.symbols = '@#$%!.[]{}=+-_';
   if (!Action.preferences.minLength)
     Action.preferences.minLength = 15;
-  if (!Action.preferences.numberCount)
-    Action.preferences.numberCount = 2;
-  if (!Action.preferences.symbolCount)
-    Action.preferences.symbolCount = 1;
+  if (!Action.preferences.lowercaseMin)
+    Action.preferences.lowercaseMin = 2;
+  if (!Action.preferences.uppercaseMin)
+    Action.preferences.uppercaseMin = 2;
+  if (!Action.preferences.numberMin)
+    Action.preferences.numberMin = 2;
+  if (!Action.preferences.symbolMin)
+    Action.preferences.symbolMin = 1;
   if (!Action.preferences.showAlert)
     Action.preferences.showAlert = 'yes';
 }
@@ -30,27 +35,33 @@ function rand(possible) {
 function run() {
 
   setup();
-  var numberCount = 0;
-  var symbolCount = 0;
+  var lowercaseMin = 0;
+  var uppercaseMin = 0;
+  var numberMin = 0;
+  var symbolMin = 0;
   
   var type = "";
   var pwd = "";
   // don't start or end on a symbol
   while (pwd.length < Action.preferences.minLength
-      || numberCount < Action.preferences.numberCount
-      || symbolCount < Action.preferences.symbolCount
+      || lowercaseMin < Action.preferences.lowercaseMin
+      || uppercaseMin < Action.preferences.uppercaseMin
+      || numberMin < Action.preferences.numberMin
+      || symbolMin < Action.preferences.symbolMin
       || type === "S") {
     type = rand(TYPES);
-    if ("L" === type) {
+    if ("L" === type && Action.preferences.lowercaseMin >= 0) {
       pwd += rand(Action.preferences.lowercase);
-    } else if ("U" === type) {
+      lowercaseMin++;
+    } else if ("U" === type && Action.preferences.uppercaseMin >= 0) {
       pwd += rand(Action.preferences.uppercase);
-    } else if ("N" === type && numberCount < Action.preferences.numberCount) {
+      uppercaseMin++;
+    } else if ("N" === type && Action.preferences.numberMin >= 0) {
       pwd += rand(Action.preferences.numbers);
-      numberCount++;
-    } else if (pwd.length > 0 && "S" === type && symbolCount < Action.preferences.symbolCount) {
+      numberMin++;
+    } else if ("S" === type && Action.preferences.symbolMin >= 0 && pwd.length > 0) {
       pwd += rand(Action.preferences.symbols);
-      symbolCount++;
+      symbolMin++;
     }    
   }
   LaunchBar.setClipboardString(pwd);
