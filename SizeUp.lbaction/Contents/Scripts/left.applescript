0966c455b0f353f3f55f23ee63d42b770480ae04
Padraic Renaghan <prenagha@renaghan.com>
2018-08-06 10:57:38 -0400

    more work on sizeup

---
+++ SizeUp.lbaction/Contents/Scripts/left.applescript
@@ -0,0 +1,16 @@
+on dlog(str)
+	set msg to "LaunchBar.SizeUp.AppleScript: " & str
+	log quoted form of msg
+	do shell script "logger " & quoted form of msg
+end dlog
+
+dlog("ascript start")
+try
+	
+	tell application "SizeUp" to do action Right
+	
+on error errorMessage number errorNumber
+	dlog("Error: " & errorMessage & ", ErrorNumber: " & errorNumber)
+end try
+
+dlog("ascript end")
--- SizeUp.lbaction/Contents/Scripts/sizeup.js
+++ SizeUp.lbaction/Contents/Scripts/sizeup.js
@@ -8,13 +8,14 @@ COMMANDS.push({'title': 'Up', 'arg': 'Up', 'icon': 'align-up@2x.png'});
 COMMANDS.push({'title': 'Down', 'arg': 'Down', 'icon': 'align-down@2x.png'});
 
 function run() {
+  LaunchBar.log("SizeUp LBAction");
   output = [];  
   for (var i = 0; i < COMMANDS.length; i++) {
     c = COMMANDS[i];
     output.push(
       {'title': c.title
       ,'icon': 'com.irradiatedsoftware.SizeUp:' + c.icon
-      ,'action': 'go'
+      ,'action': 'ascript'
       ,'actionArgument': c.arg
       ,'actionRunsInBackground': true
     });
@@ -22,15 +23,22 @@ function run() {
   return output;
 }
 
-function go(command) {
+function ascript(command) {
+  try {
+    //var c = 'tell application "SizeUp" to do action ' + command;
+    //LaunchBar.debugLog(c);
+    //LaunchBar.executeAppleScript(c);
+    LaunchBar.debugLog("LB applescript start");
+    LaunchBar.executeAppleScriptFile("left.applescript");
+    LaunchBar.debugLog("LB applescript done");
+  } catch (exception) {
+    return err('AppleScript Exception: ' + command + ' -- ' + exception);
+  }
 }
 
-function err(msg, file) {
+function err(msg) {
   var m = 'ERROR: ' + msg;
   LaunchBar.log(m);
-  return [{'title': m, 
-    'icon': ALERT_ICON, 
-    path: file, 
-    subtitle: file}];
+  return [{'title': m, 'icon': ALERT_ICON}];
 }
  
