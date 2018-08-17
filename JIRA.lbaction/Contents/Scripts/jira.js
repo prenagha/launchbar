b324576e7dba098763cd439f195b5783f79a9ef2
Padraic Renaghan <prenagha@renaghan.com>
2018-08-17 10:12:25 -0400

    configurable jira urls

--- JIRA.lbaction/Contents/Scripts/jira.js
+++ JIRA.lbaction/Contents/Scripts/jira.js
@@ -1,22 +1,23 @@
 
-var HOST = "http://cscerdevnor001.fsg.amer.csc.com";
-var JIRA_HOST = HOST + ":8080";
-var CRUCIBLE_HOST = HOST + ":8060";
-
 function run(arg) {
   if (!arg || arg === null || typeof(arg) != 'string' || arg.length === 0)
     return;
   
+  if (!Action.preferences.jiraBase)
+    Action.preferences.jiraBase = "https://jira.example.com";
+  if (!Action.preferences.crucibleBase)
+    Action.preferences.crucibleBase = "https://crucible.example.com";
+    
   var url = "";
   if (arg.match(/^\d+$/)) {
-    url = JIRA_HOST + "/browse/ER-" + arg;
+    url = Action.preferences.jiraBase + "/browse/ER-" + arg;
   } else if (arg.match(/^ER-\d+$/i)) {
-    url = JIRA_HOST + "/browse/" + arg;
+    url = Action.preferences.jiraBase + "/browse/" + arg;
   } else if (arg.match(/^CR-\d+$/i)) {
-    url = CRUCIBLE_HOST + "/cru/" + arg;
+    url = Action.preferences.crucibleBase + "/cru/" + arg;
   } else {
     var term = encodeURIComponent(arg);
-    url = JIRA_HOST + "/issues/?jql=(summary%20~%20%22" 
+    url = Action.preferences.jiraBase + "/issues/?jql=(summary%20~%20%22" 
       + term + "%22%20or%20description%20~%20%22" 
       + term + "%22)%20and%20status%20!%3D%20Closed%20order%20by%20key%20desc";
   }
