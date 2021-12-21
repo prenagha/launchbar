73eda166a554a59573fb3881e5eb0ab090a87711
Padraic Renaghan <padraic@renaghan.com>
2021-12-21 10:30:47 -0500

    configurable default project and query removed crucible
    idea from alloydwhitlock PR #18

--- JIRA.lbaction/Contents/Scripts/jira.js
+++ JIRA.lbaction/Contents/Scripts/jira.js
@@ -2,24 +2,39 @@
 function run(arg) {
   if (!arg || arg === null || typeof(arg) != 'string' || arg.length === 0)
     return;
+
+  arg = arg.trim();
   
+  // default jira project  
+  if (!Action.preferences.jiraDefaultProject)
+    Action.preferences.jiraDefaultProject = "";
+    
+  // base URL to jira server
   if (!Action.preferences.jiraBase)
     Action.preferences.jiraBase = "https://jira.example.com";
-  if (!Action.preferences.crucibleBase)
-    Action.preferences.crucibleBase = "https://crucible.example.com";
-    
+
+  // text search query, <q> replaced with the search term
+  if (!Action.preferences.jiraQuery)
+    Action.preferences.jiraQuery = "/issues/?jql=text%20~%20%22<q>%22"
+      +"%20AND%20status%20!%3D%20Closed%20ORDER%20BY%20key%20DESC";
+      
   var url = "";
-  if (arg.match(/^\d+$/)) {
-    url = Action.preferences.jiraBase + "/browse/ER-" + arg;
-  } else if (arg.match(/^ER-\d+$/i)) {
+  // number assumed to be jira issue id in default project
+  if (Action.preferences.jiraDefaultProject.length > 0 && arg.match(/^\d+$/)) {
+    url = Action.preferences.jiraBase 
+        + "/browse/" 
+        + Action.preferences.jiraDefaultProject 
+        + "-" + arg;
+
+  // XX-number assumed to be a fully qualified jira issue id
+  } else if (arg.match(/^[A-Z]{2,}-\d+$/i)) {
     url = Action.preferences.jiraBase + "/browse/" + arg;
-  } else if (arg.match(/^CR-\d+$/i)) {
-    url = Action.preferences.crucibleBase + "/cru/" + arg;
+    
+  // otherwise string assumed to be a search term
   } else {
-    var term = encodeURIComponent(arg);
-    url = Action.preferences.jiraBase + "/issues/?jql=(summary%20~%20%22" 
-      + term + "%22%20or%20description%20~%20%22" 
-      + term + "%22)%20and%20status%20!%3D%20Closed%20order%20by%20key%20desc";
+    const term = encodeURIComponent(arg);
+    url = Action.preferences.jiraBase 
+        + Action.preferences.jiraQuery.replaceAll('<q>', term);
   }
       
   LaunchBar.debugLog("URL " + url);
