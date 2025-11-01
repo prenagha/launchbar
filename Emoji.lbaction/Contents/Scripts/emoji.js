630b5c2e1ad7f04d8cf62deefbbe249f464ea1c1
Padraic Renaghan <padraic@renaghan.com>
2025-10-31 22:04:48 -0400

    documentation

--- Emoji.lbaction/Contents/Scripts/emoji.js
+++ Emoji.lbaction/Contents/Scripts/emoji.js
@@ -84,6 +84,8 @@ function compareFrequent(a, b) {
 
 function runWithString(input) {
   var result = [];
+  
+  // load the JSON data files
   var emojiKeywords = getJsonData(result, "emoji-en-US");
   var emojiUnicode = getJsonData(result, "data-by-emoji");
   var emojiComponents = getJsonData(result, "data-emoji-components");
@@ -91,6 +93,7 @@ function runWithString(input) {
 
   if (result.length > 0) return result;
 
+  // process usages into frequency which we will then sort
   var frequents = [];
   for (const [emoji, usage] of Object.entries(usages)) {
     frequents.push({
@@ -99,7 +102,8 @@ function runWithString(input) {
       "emoji":   emoji
     });
   }
-  
+
+  // add pref favorites if any to become top usages  
   const favs = favorites().split(" ");
   const now = new Date().toISOString();
   var f = 0;
@@ -111,17 +115,23 @@ function runWithString(input) {
       "emoji":   fav
     });
   }
+  
+  // sort usages by count DESC, newest date last used, emoji
   frequents.sort(compareFrequent);
   
   const query = input == undefined || !input ? "" : input.trim().toLowerCase();
 
+  // find any frequent usages that match
+  // empty query will match all frequent usages
   for (const frequent of frequents) {
     const keywords = emojiKeywords[frequent.emoji];
     emojiMatchResult(result, query, emojiUnicode, emojiComponents, frequent.emoji, keywords); 
   }
 
+  // if query not empty, check against all other emoji
   if (query.length > 0) {
     for (const [emoji, keywords] of Object.entries(emojiKeywords)) {
+      // skip if a frequent usage as already processed
       if (usages[emoji]) continue;
       emojiMatchResult(result, query, emojiUnicode, emojiComponents, emoji, keywords);   
     }
@@ -133,12 +143,15 @@ function runWithString(input) {
   return result;
 }
 
+// check if an emoji matches query and if so add as a result
 function emojiMatchResult(result, query, emojiUnicode, emojiComponents, emoji, keywords) {
   const keyword = emojiMatch(query, keywords);
   if (keyword != undefined)
     emojiResult(result, emojiUnicode, emojiComponents, emoji, keyword);   
 }
 
+// check if query matches emoji keywords
+// return matched query if a match, otherwise null
 function emojiMatch(query, keywords) {
   if (!keywords) return null;
   if (query.length === 0) return "";
@@ -148,6 +161,7 @@ function emojiMatch(query, keywords) {
   return null;
 }
 
+// add a matched emoji as a LaunchBar result
 function emojiResult(result, emojiUnicode, emojiComponents, emoji, keyword) {
   const info = emojiUnicode[emoji];
   const match = {
@@ -156,14 +170,15 @@ function emojiResult(result, emojiUnicode, emojiComponents, emoji, keyword) {
     "subtitle": keyword,
     "action":   'selectedEmoji',
     "actionArgument": {
-      "name":    info.name,
-      "slug":    info.slug,
       "emoji":   emoji
     }
   };
+  // if emoji supports skin tone and we have a pref skin tone
+  // then apply skin tone to emoji
   const skinToneName = skinTone();
   if (skinToneName && skinToneName.length > 0 && info.skin_tone_support) {
     const skinTone = emojiComponents[skinToneName];
+    // show original emoji and skin tone as badge
     match["badge"] = emoji + " " + skinTone;
     const variation = emoji + skinTone;
     match["icon"] = variation;          
@@ -172,6 +187,9 @@ function emojiResult(result, emojiUnicode, emojiComponents, emoji, keyword) {
   result.push(match);
 }
 
+// called after search result is selected
+// add selection as new usage
+// paste selection to front application
 function selectedEmoji(selection) {
   const result = [];
   const usages = readUsages(result);
